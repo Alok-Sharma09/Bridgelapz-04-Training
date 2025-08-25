faa6a179a33a41d502330b1764561ee6d1170ccf
Alok-Sharma09 <alok202006@gmail.com>
2025-08-25 15:16:55 +0530

    Add files via upload

---
+++ programming_elements/Sam_Average_Marks.java
@@ -0,0 +1,13 @@
+package programming_elements;
+
+public class Sam_Average_Marks {
+	public static void main (String[]args) {
+		int Maths = 94;
+		int Physics = 95;
+		int Chemistry = 96;
+		int Average = (Maths + Physics + Chemistry)/3 ;
+		
+		System.out.println("Sam's Average marks in PCM is " +Average);
+	}
+
+}
